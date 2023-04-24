11930794973a6f4e455a6492910afed343965577
aaronnieto <80398559+aaronnieto@users.noreply.github.com>
2023-04-24 18:06:13 +0200

    Add files via upload
    menu basico del trabajo

---
+++ trabajo2223.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int opcionMenu;
+	do{
+	
+	printf("BIENVENIDO\n¿QUE DESEA HACER?\n1.REGISTRAR NUEVO FICHERO\n2.REVISAR FICHEROS SUBIDOS\n3.INSTRUCCIONES Y AYUDA\n4.SALIR\n");
+	scanf("%d",&opcionMenu);
+	switch (opcionMenu){
+
+	    case 1:
+	    	printf("HAS ELEGIDO REGISTRAR UN NUEVO FICHERO\n");
+	    	break;
+	    case 2:
+	    	printf("HAS ELEGIDO REVISAR FICHEROS SUBIDOS\n");
+	    	FILE * fentrada;
+						fentrada = fopen("ficherosguardados.txt", "r");
+						    if(fentrada == NULL){
+							    printf("Error en la apertura del fichero\n");
+			return 0;
+		}
+	    	break;
+	    case 3 :
+	    	printf("HAS ELEGIDO INSTRUCCIONES Y AYUDA\n");
+	    	break;
+	    case 4:
+	    	printf("HASTA PRONTO\n");
+	    	break;
+        }
+	    	
+
+    }while(opcionMenu!=4);
+  
+  
+  return 0;
+}

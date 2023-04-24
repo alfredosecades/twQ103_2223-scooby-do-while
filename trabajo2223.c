338d381f58285fff42e23d4acea340f409e271d2
aaronnieto <aaron.nieto@alumnos.upm.es>
2023-04-24 19:50:23 +0200

    Update trabajo2223.c
    crea ficheros con el nombre que quieras

--- trabajo2223.c
+++ trabajo2223.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 
+
+
 int main(){
 	int opcionMenu;
+	char orden;
+	char nombrefic[20];
+	char texto[100];  
+		  
 	do{
 	
 	printf("BIENVENIDO\n¿QUE DESEA HACER?\n1.REGISTRAR NUEVO FICHERO\n2.REVISAR FICHEROS SUBIDOS\n3.INSTRUCCIONES Y AYUDA\n4.SALIR\n");
@@ -10,18 +16,36 @@ int main(){
 	switch (opcionMenu){
 
 	    case 1:
-	    	printf("HAS ELEGIDO REGISTRAR UN NUEVO FICHERO\n");
-	    	break;
+	    	
+	    	printf("introduce como quieres llamar al archivo fichero\n");
+		  scanf("%s", nombrefic);
+		  
+		    FILE *fsalida;
+	        fsalida = fopen(nombrefic, "w");
+        	if (fsalida == NULL) {
+	    	    printf("Error, no puede crear el fichero.\n");
+	        	return 0;	
+	        }
+        	printf("que desea introducir\n");
+        	fflush(stdin);
+	        fgets(texto, 100, stdin);	
+	        fclose(fsalida);
+            printf("Se ha escrito el archivo correctamente.\n");
+            break;
+             
+         
 	    case 2:
+	    	/*
 	    	printf("HAS ELEGIDO REVISAR FICHEROS SUBIDOS\n");
-	    	FILE * fentrada;
-						fentrada = fopen("fichero1.txt", "r");
-						if(fentrada == NULL){
+	    	FILE * fentrada2;
+			fentrada2 = fopen("fichero1.txt", "w");
+						if(fentrada2 == NULL){
 							printf("Error en la apertura del fichero\n");
 							return 0;
 						}
 		    printf("El archivo se abrio correctamente\n");
-            fclose(fentrada);
+            fclose(fentrada2);
+            */
 	    	break;
 	    case 3 :
 	    	printf("HAS ELEGIDO INSTRUCCIONES Y AYUDA\n");
@@ -30,6 +54,7 @@ int main(){
 	    	printf("HASTA PRONTO\n");
 	    	break;
         }
+    
 	    	
 
     }while(opcionMenu!=4);
